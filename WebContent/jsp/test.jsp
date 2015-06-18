1f910dcf21168206ba5f830774d860a6d76c6291
mazdazz <mazda_m_m@yahoo.co.jp>
2015-06-18 19:22:43 +0900

    add web project

---
+++ WebContent/jsp/test.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/qrcode_reader_web/Main" enctype="multipart/form-data" method="post"></form>
+<h1>SampleTest2</h1>
+<input type="file" name="fileUploader" size="50">
+<input type="submit" value="upload">
+</form>
+</body>
+</html>
---
+++ src/qrcode_reader/Main.java
@@ -0,0 +1,49 @@
+package qrcode_reader;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Main() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jspFilePath = "/jsp/test.jsp";
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher(jspFilePath);
+		dispatch.forward(request, response);
+		
+//		PrintWriter writer = response.getWriter();
+//		
+//		writer.println("test");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("call post method");
+	}
+
+}
